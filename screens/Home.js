237628614e101118c6588fcac2ec4fd8028b3fda
Antonio Dambujo <pdambuja@gmail.com>
2022-05-29 10:49:17 +0200

    search bar layout

--- screens/Home.js
+++ screens/Home.js
@@ -8,12 +8,16 @@ import {
 } from 'react-native';
 import React from 'react';
 import HeaderTab from '../components/HeaderTab';
+import SearchBar from '../components/SearchBar';
 
 export default function Home() {
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ backgroundColor: '#eee', flex: 1 }}>
       <StatusBar />
-      <HeaderTab />
+      <View style={{ backgroundColor: 'white', padding: 15 }}>
+        <HeaderTab />
+        <SearchBar />
+      </View>
     </SafeAreaView>
   );
 }
